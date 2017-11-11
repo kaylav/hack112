b13ee9eddda894674228ed6717503f3774a82608
Kayla Vokt <kaylavokt@Kaylas-MBP.wv.cc.cmu.edu>
2017-11-11 17:19:33 -0500

    starting subclass

---
+++ test.py
@@ -0,0 +1,6 @@
+#write the inner circle subclass of OuterCircle
+#should include the function grow, is at a constant ratio from the outer radius
+
+class InnerCircle(Circle):
+    
+    def __init__(self, radius)
